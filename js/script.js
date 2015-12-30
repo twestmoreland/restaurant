1021d5cb1958b55ea33105ba98419ea501ab6016
Tyler Westmoreland <tyler@westmoreland.us>
2015-12-30 09:19:02 -0500

    Sidenaving is fixed and new stylesheet is added

--- js/script.js
+++ js/script.js
@@ -36,11 +36,106 @@ $(document).ready(function(){
 	    });
 	};
 
-	$('.sidenav').followTo(1072);
+	$('.sidenav').followTo(1066);
 
-
-	$( "aside > ul > li > a" ).click(function() {
-	 	alert( "Handler for .click() called." );
-	 	('body').scrollTop(6000);
+	$(".tongs").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#tongs").offset().top
+	    }, 2000);
+	    return false;
 	});
+
+	$(".spatula").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#spatula").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".thermometer").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#thermometer").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".grillbrush").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#grillbrush").offset().top
+	    }, 2000);
+	    return false;
+	});
+	
+	$(".grillinggloves").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#grillinggloves").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".tongs").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#tongs").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".bastingbrush").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#bastingbrush").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".charcoalchimney").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#charcoalchimney").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".grillingfork").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#grillingfork").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".garlicpress").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#garlicpress").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".wineopener").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#wineopener").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".canopener").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#canopener").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".culinarytorch").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#culinarytorch").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+	$(".saladspinner").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#saladspinner").offset().top
+	    }, 2000);
+	    return false;
+	});
+
+
+
 });
